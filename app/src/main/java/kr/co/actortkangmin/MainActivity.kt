3c733d18bb026a99e9efd5c772850b3153ca9c63
JISOO2206 <900522z@gmail.com>
2023-01-08 18:51:25 +0900

    사진 레이아웃 수정, 사진 클릭시 텍스트 표출 이벤트 생성

--- app/src/main/java/kr/co/actortkangmin/MainActivity.kt
+++ app/src/main/java/kr/co/actortkangmin/MainActivity.kt
@@ -2,10 +2,29 @@ package kr.co.actortkangmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import android.widget.Toast.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 궁금한 점
+        // 만약 데이터로 가져오는 사진의 경우 어떻게 동적으로 이벤트가 되도록 하는가
+
+        // 1. 화면이 클릭되었다는 것을 알아야 함.
+        val photo1 = findViewById<ImageView>(R.id.photo_1)
+
+        // 사진이 클릭되면 어떻게 동작할 것인가
+        photo1.setOnClickListener {
+            Toast.makeText(this, "1번 사진 클릭", Toast.LENGTH_LONG).show()
+        }
+        // 2. 화면이 클릭되면 다음 화면으로 넘어가서 사진을 크게 보여줌.
+
+        // 3.
     }
 }
