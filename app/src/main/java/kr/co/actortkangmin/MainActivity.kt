e624e01d43f206fbdbd30e4b197a618a6e04ea0f
JISOO2206 <900522z@gmail.com>
2023-01-08 19:15:10 +0900

    모든 사진 클릭시 사진이 크게 보이는 화면으로 전환이 되도록 이벤트 생성

--- app/src/main/java/kr/co/actortkangmin/MainActivity.kt
+++ app/src/main/java/kr/co/actortkangmin/MainActivity.kt
@@ -20,6 +20,14 @@ class MainActivity : AppCompatActivity() {
 
         // 1. 화면이 클릭되었다는 것을 알아야 함.
         val photo1 = findViewById<ImageView>(R.id.photo_1)
+        val photo2 = findViewById<ImageView>(R.id.photo_2)
+        val photo3 = findViewById<ImageView>(R.id.photo_3)
+        val photo4 = findViewById<ImageView>(R.id.photo_4)
+        val photo5 = findViewById<ImageView>(R.id.photo_5)
+        val photo6 = findViewById<ImageView>(R.id.photo_6)
+        val photo7 = findViewById<ImageView>(R.id.photo_7)
+        val photo8 = findViewById<ImageView>(R.id.photo_8)
+        val photo9 = findViewById<ImageView>(R.id.photo_9)
 
         // 사진이 클릭되면 어떻게 동작할 것인가
         photo1.setOnClickListener {
@@ -29,7 +37,45 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        photo2.setOnClickListener {
+            val intent = Intent(this, ActorPhotoActivity3::class.java)
+            startActivity(intent)
+        }
 
+        photo3.setOnClickListener {
+            val intent = Intent(this, ActorPhotoActivity4::class.java)
+            startActivity(intent)
+        }
+
+        photo4.setOnClickListener {
+            val intent = Intent(this, ActorPhotoActivity5::class.java)
+            startActivity(intent)
+        }
+
+        photo5.setOnClickListener {
+            val intent = Intent(this, ActorPhotoActivity6::class.java)
+            startActivity(intent)
+        }
+
+        photo6.setOnClickListener {
+            val intent = Intent(this, ActorPhotoActivity7::class.java)
+            startActivity(intent)
+        }
+
+        photo7.setOnClickListener {
+            val intent = Intent(this, ActorPhotoActivity8::class.java)
+            startActivity(intent)
+        }
+
+        photo8.setOnClickListener {
+            val intent = Intent(this, ActorPhotoActivity9::class.java)
+            startActivity(intent)
+        }
+
+        photo9.setOnClickListener {
+            val intent = Intent(this, ActorPhotoActivity10::class.java)
+            startActivity(intent)
+        }
 
         // 3.
     }
