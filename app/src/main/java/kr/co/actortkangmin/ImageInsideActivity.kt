198379c1e15bc16645b6713f1510aedf4d54cb3f
JISOO2206 <900522z@gmail.com>
2023-01-10 22:19:46 +0900

    사진 클릭시 이동 로직에 대한 리팩토링으로 인한 커밋 및 activity_image_inside 파일 생성

--- app/src/main/java/kr/co/actortkangmin/ImageInsideActivity.kt
+++ app/src/main/java/kr/co/actortkangmin/ImageInsideActivity.kt
@@ -3,9 +3,9 @@ package kr.co.actortkangmin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ActorPhotoActivity5 : AppCompatActivity() {
+class ImageInsideActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_actor_photo5)
+        setContentView(R.layout.activity_image_inside)
     }
 }
