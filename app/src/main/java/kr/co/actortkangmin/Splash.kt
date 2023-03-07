7e60bb046dfa0e640fe13ae2a624965dc2f2ffc0
JISOO2206 <900522z@gmail.com>
2023-03-07 23:06:46 +0900

    메서드 정보 주석으로 추가

--- app/src/main/java/kr/co/actortkangmin/Splash.kt
+++ app/src/main/java/kr/co/actortkangmin/Splash.kt
@@ -11,6 +11,7 @@ class Splash : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        // Handler().postDelayed : n 초후에 실행 가능하도록 하는 메서드
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
